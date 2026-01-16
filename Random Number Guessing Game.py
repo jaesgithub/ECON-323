0d0896081bd0bfbb917f3fb61b966b069e7ae958
Jaewoong Shin <48845130+jaesgithub@users.noreply.github.com>
2026-01-15 22:47:06 -0800

    Creating games during lecture because I was bored.

---
+++ Random Number Guessing Game.py
@@ -0,0 +1,8 @@
+import random
+
+n = random.randint(1, 100)
+i = random.randint(1, 100)
+
+print("Welcome to the Random Number Guessing Game!")
+print("I have selected a random number between 1 and 100.")
+print("Try to guess the number!")
