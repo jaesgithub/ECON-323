85e21ad47170266103350230c35da421556b59c3
Jaewoong Shin <48845130+jaesgithub@users.noreply.github.com>
2026-01-16 06:51:43 +0000

    Unfinished

--- Random Number Guessing Game.py
+++ Random Number Guessing Game.py
@@ -6,3 +6,7 @@ i = random.randint(1, 100)
 print("Welcome to the Random Number Guessing Game!")
 print("I have selected a random number between 1 and 100.")
 print("Try to guess the number!")
+
+while n != i:
+    print(n)
+    n = random.randint(1, 100)
